f0a2e967adda2f6c67ea485ef874eaa07853ff73
Eva <eombaffa@gmail.com>
2022-03-08 11:13:25 +0100

    Frist Commit

--- src/app/classes/hacker.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Hacker } from './hacker';
-
-describe('Hacker', () => {
-  it('should create an instance', () => {
-    expect(new Hacker()).toBeTruthy();
-  });
-});
--- src/app/classes/hacker.ts
+++
@@ -1,9 +0,0 @@
-export class Hacker {
-    constructor(
-
-        public ip: string,
-        public country_name: string,        
-        public region_name: string,       
-        public city: string 
-        ) { }
-}
